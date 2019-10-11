99d61c384be3f0a9bc1bdb185676d137e305049b
Akatosh, the God of Time and Order <borjan_blizz@hotmail.com>
2019-10-11 20:04:14 +0200

    Change folder structure

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Route } from 'react-router';
 
-import Layout from './components/Layout';
-import Home from './components/Home';
+import Layout from './components/Layout/Layout';
+import Home from './components/Default/Home';
 import Games from './components/Games/Games';
 import Game from './components/Games/Game';
 
