f5716f7adaf57bce76e5502cc6784b98584dee41
Stendarr,  God of Righteous Might and Merciful Forbearance <47365010+AzureSavant@users.noreply.github.com>
2019-10-16 21:49:18 +0200

    Added NavBar functionalities (#2)
    * Added NavBar functionalities
    
    * Add NavBar styling

--- ClientApp/src/components/Layout/Layout.js
+++ ClientApp/src/components/Layout/Layout.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Container } from 'reactstrap';
+
 import NavMenu from './NavMenu';
 
 export default props => (
--- ClientApp/src/scenes/Layout/components/Header.js
+++ ClientApp/src/scenes/Layout/components/Header.js
@@ -1,4 +1,5 @@
 ﻿import React from 'react';
+import { NavLink, Link } from 'react-router-dom';
 
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
@@ -8,15 +9,32 @@ import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 
-const useStyles = makeStyles(theme => ({
+
+const useStyles = makeStyles(theme => ({    
     root: {
         flexGrow: 1,
     },
     menuButton: {
         marginRight: theme.spacing(2),
+        
     },
     title: {
         flexGrow: 1,
+        textDecoration:'none',
+        color:'white',
+        '&:hover':{
+            textDecoration:'none',
+            color:'white',
+        },
+    },
+    button:{
+        color:'white',
+        '&:hover':{
+            color:'white',
+        },
+        '& a:hover':{
+            color:'white',
+        },
     },
 }));
 
@@ -30,12 +48,12 @@ export default function Header() {
                     <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
                         <MenuIcon />
                     </IconButton>
-                    <Typography variant="h6" className={classes.title}>
-                        The Way Home
+                    <Typography variant="h6" className={classes.title} color="inherit" component={Link} to="/" exact >
+                            The Way Home
                     </Typography>
-                    <Button color="inherit">Play</Button>
-                    <Button color="inherit">Rules</Button>
-                    <Button color="inherit">News</Button>
+                    <Button className={classes.button} color="inherit" component={NavLink} to="/games" exact >Play </Button>
+                    <Button className={classes.button} color="inherit" component={NavLink} to="/about" exact >Rules</Button>
+                    <Button className={classes.button} color="inherit" component={NavLink} to="/news" exact  >News</Button>
                 </Toolbar>
             </AppBar>
         </div>
